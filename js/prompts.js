9c66f3ca8b2a037803f80e992c5d86ac016c4b70
Kevin L Wei <public@kevinlwei.com>
2019-07-11 02:56:20 -0400

    changes to content and updated prompts

--- js/prompts.js
+++ js/prompts.js
@@ -9,9 +9,9 @@ cats: display ascii art of cats ;)
 *invalid*: text to display when no valid command is entered
 */
 
-var intro = `I'm a <span class="special">political organizer</span> and <span class="special">policy researcher</span> with a technical background. My main policy interests are economic inequality, technology regulation and innovation policy, and select microeconomics topics. Right now I'm a Summer Associate at <a href="https://digitalocean.com">DigitalOcean</a>, where I work on education equity and diversity.
+var intro = `I'm a <span class="special">political organizer</span> and <span class="special">policy researcher</span> with a technical background. My main policy interests are economic inequality, technology regulation and innovation policy, and select microeconomics topics. Right now I'm an Associate at <a href="https://digitalocean.com" target="_blank">DigitalOcean</a>, where I work on education equity and diversity.
 \t 
-\tIn the past, I was a 2019 State of the Net Fellow at the <a href="https://www.neted.org/">Internet Education Foundation</a> and a Beto Senate campaign staffer. I've also worked/interned in the Office of Nancy Pelosi, the <a href="https://www.cfr.org/">Council of Foreign Relations</a>, the <a href="http://www.techstars.com/programs/">Techstars IoT Accelerator</a>, the <a href="https://www.georgewbushlibrary.smu.edu/">George W. Bush Presidential Library</a>, and the <a href="https://www.si.edu/">Smithsonian</a>. When time permits, I'm an avid Overwatch player (TeSPA) and science fiction fan (reviewer at <a href="http://www.fantasyliterature.com/">FantasyLiterature.com</a>). Studying Mathematics-Statistics & Economics at <a href="https://www.columbia.edu/">Columbia</a> (expected 2019) with some IR/politics.
+\tIn the past, I was a 2019 State of the Net Fellow at the <a href="https://www.neted.org/" target="_blank">Internet Education Foundation</a> and a Beto Senate campaign staffer. I've also worked/interned in the Office of Nancy Pelosi, the <a href="https://www.cfr.org/" target="_blank">Council of Foreign Relations</a>, the <a href="http://www.techstars.com/programs/" target="_blank">Techstars IoT Accelerator</a>, the <a href="https://www.georgewbushlibrary.smu.edu/" target="_blank">George W. Bush Presidential Library</a>, and the <a href="https://www.si.edu/" target="_blank">Smithsonian</a>. When time permits, I'm an avid Overwatch player (TeSPA) and science fiction fan (reviewer at <a href="http://www.fantasyliterature.com/" target="_blank">FantasyLiterature.com</a>). Recent graduate of Columbia, where I studied Mathematics-Statistics & Economics with some IR/politics.
 \t 
 \t To learn more about me, type <span class="special">social</span> to find me around the web, <span class="special">portfolio</span> to see my past work, or <span class="special">contact</span> to reach out to me.`;
 
@@ -41,21 +41,22 @@ var help = [{
   description: "Clear the screen"
 }];
 
-var social = `<a href="https://www.linkedin.com/in/kevinlwei/">LinkedIn</a>
-\t<a href="https://twitter.com/kevinlwei">Twitter</a>
-\t<a href="https://angel.co/kevinwei">AngelList</a>
+var social = `<a href="https://www.linkedin.com/in/kevinlwei/" target="_blank">LinkedIn</a>
+\t<a href="https://twitter.com/kevinlwei" target="_blank">Twitter</a>
+\t<a href="https://angel.co/kevinwei" target="_blank">AngelList</a>
 \t 
 \t Type <span class = "special">help</span> for more commands.`;
 
-var portfolio = `<a href="https://www.fcc.gov/ecfs/filing/10717079873938">Net Neutrality Comment</a>: here's internet infrastructure startup DigitalOcean's comment to the FCC in support of net neutrality, which I wrote back in 2017
-\t<a href="http://www.fantasyliterature.com/author-interviews/n-k-jemisin/">N.K. Jemisin Interview</a>: here's me interviewing NYT-bestselling fantasy author N.K. Jemisin about her Broken Earth trilogy
-\t<a href="https://impulse.coreatcu.com/libretaxi-an-uber-for-rural-disadvantaged-communities-657269849d31">The Story of LibreTaxi</a>: here's me writing about LibreTaxi, a non-profit ride-sharing app aimed at rural users
-\t<a href="https://medium.com/@kevinwei">Medium</a>: my writing, mostly for Columbia's tech- & startup-focused Core Impulse magazine
-\t<a href="https://github.com/kevinlwei">GitHub</a>: my code snippets :)
+var portfolio = `<a href="https://www.fcc.gov/ecfs/filing/10717079873938" target="_blank">Net Neutrality Comment</a> (2017): here's internet infrastructure startup DigitalOcean's comment to the FCC in support of net neutrality, which I wrote back in 2017
+\t<a href="https://fbnewsroomus.files.wordpress.com/2019/06/oversight-board-consultation-report-appendix.pdf" target="_blank">Facebook Global Oversight Board Comment</a> (2019): here's my personal comment to Facebook on their proposed structure for a "Supreme Court" of content moderation
+\t<a href="http://www.fantasyliterature.com/author-interviews/n-k-jemisin/" target="_blank">N.K. Jemisin Interview</a> (2016): here's me interviewing NYT-bestselling fantasy author N.K. Jemisin about her Broken Earth trilogy
+\t<a href="https://impulse.coreatcu.com/libretaxi-an-uber-for-rural-disadvantaged-communities-657269849d31" target="_blank">The Story of LibreTaxi</a> (2017): here's me writing about LibreTaxi, a non-profit ride-sharing app aimed at rural users
+\t<a href="https://medium.com/@kevinwei" target="_blank">Medium</a>: my writing, mostly for Columbia's tech- & startup-focused Core Impulse magazine
+\t<a href="https://github.com/kevinlwei" target="_blank">GitHub</a>: my code snippets :)
 \t 
 \t Type <span class = "special">help</span> for more commands.`;
 
-var contact = `Feel free to reach out at <a href="mailto:hi@kevinlwei.com">hi@kevinlwei.com</a>. I'm always down to grab some #covfefe, collaborate on projects, or provide feedback on what you're working on
+var contact = `Feel free to reach out at <a href="mailto:hi@kevinlwei.com" target="_blank">hi@kevinlwei.com</a>. I'm always down to grab some #covfefe, collaborate on projects, or provide feedback on what you're working on
 \t 
 \t Type <span class = "special">help</span> for more commands.`;
 
